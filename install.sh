3d1c27628563ee3e70f046ae24dda1a4cfee0453
jaymarnz <me@jaymarnz.dev>
2025-03-20 11:41:27 +1300

    verify install script is running as root

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Make sure we are running as root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
 # determine the directory where this script is located
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
